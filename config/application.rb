d040d6ee556dde270b018a3f81a93d8f51cbc16f
EC2 Default User <ec2-user@ip-172-31-47-203.ap-northeast-1.compute.internal>
2024-01-08 12:53:02 +0000

    ２章途中

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+config.hosts << "62b14c59766f443395fe39c313bb41e7.vfs.cloud9.ap-northeast-1.amazonaws.com"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+
 Rails.application.routes.draw do
+
+  get '/top' => 'homes#top'
+  get 'lists/new', to:'lists#new'
+  post 'lists' => 'lists#create'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
