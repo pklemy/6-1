e7fd873c74f695eb21b3e7d14ea02afd85bdfba2
EC2 Default User <ec2-user@ip-172-31-47-203.ap-northeast-1.compute.internal>
2024-01-11 10:49:57 +0000

    11章途中

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,20 +1,46 @@
 class ListsController < ApplicationController
+
   def new
    @list = List.new
   end
 
   def create
-
-    list = List.new(list_params)
-
-    list.save
-
-    redirect_to '/top'
+      @list = List.new(list_params)
+    if @list.save
+        redirect_to list_path(@list.id)
+     else
+        render :new
+      end
   end
+
+
+ def index
+   @lists = List.all
+ end
+
+ def show
+    @list = List.find(params[:id])
+ end
+
+ def edit
+   @list = List.find(params[:id])
+ end
+
+ def update
+   list = List.find(params[:id])
+   list.update(list_params)
+   redirect_to list_path(list.id)
+ end
+
+ def destroy
+     list = List.find(params[:id]) #データ（レコード）を１件取得
+     list.destroy #データ（レコード）を削除
+     redirect_to '/lists' #投稿一覧画面へリダイレクト
+ end
 
  private
  #ストロングパラメーター
  def list_params
-   params.require(:list).permit(:title, :body)
+   params.require(:list).permit(:title, :body, :image)
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 
 Rails.application.routes.draw do
 
-  get '/top' => 'homes#top'
-  get 'lists/new', to:'lists#new'
-  post 'lists' => 'lists#create'
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  resources :lists
 end
